6de024565be6e672440e2ec87520a8cbcc1ba8ab
Tim Nonet <tim.nonet@gmail.com>
2021-10-14 08:13:38 -0500

    updating configuration files [9/9]
    Version update commit to mark end of configuration file changes required to get build passing on Travis CI.
    
    Root cause was miss spelling of extras_require in setup(...)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="happenings",
-    version="0.0.1",
+    version="0.0.2",
     author="Tim Nonet",
     author_email="tim.nonet@gmail.com",
     description="A utility package for creating complex event based time-series data",
